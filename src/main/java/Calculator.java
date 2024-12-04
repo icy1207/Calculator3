222522cab268ffc5c0c44a903fe6576ff37634b9
icy1207 <vladledovskikh@mail.ru>
2024-12-04 17:29:38 +0200

    initial commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -16,17 +16,17 @@ public class Calculator {
         char oper;
         String[] parts = input.split(" ");
         if (parts.length != 3) {
-            return "Вы ввели не правильное количество символов";
+            throw new IllegalArgumentException("Вы ввели неправильное количество символов. Используйте формат: 'a + b'");
         }
         try {
             num1 = Integer.parseInt(parts[0]);
             num2 = Integer.parseInt(parts[2]);
             if (num1 < 1 || num1 > 10 || num2 < 1 || num2 > 10) {
-                return "Введите числа от 1 до 10";
+                throw new IllegalArgumentException("Введите числа от 1 до 10.");
             }
             oper = parts[1].charAt(0);
         } catch (NumberFormatException e) {
-            return "Введён некорректный символ";
+            throw new IllegalArgumentException("Введены некорректные символы вместо чисел.");
         }
         switch (oper) {
             case '+':
@@ -42,13 +42,9 @@ public class Calculator {
                 result = num1 / num2;
                 break;
             default:
-                return "Неправильный знак";
+                throw new IllegalArgumentException("Неправильный знак операции. Допустимые операции: +, -, *, /.");
         }
         return num1 + " " + oper + " " + num2 + " = " + result;
     }
 }
-
-
-
-
 
